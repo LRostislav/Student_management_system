ea426445d0c23c98a53cd0a1725f60f844f8f973
LRostislav <rostislav.lysenkov24@gmail.com>
2023-08-15 23:40:47 +0300

    add controller and student mapping

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,7 +1,24 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import service.StudentService;
 
 @Controller
 public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // handler method to handle list students request and return mode and view
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
+
 }
--- src/main/java/service/impl/StudentServiceImpl.java
+++ src/main/java/service/impl/StudentServiceImpl.java
@@ -10,12 +10,11 @@ import java.util.List;
 @Service
 public class StudentServiceImpl implements StudentService {
 
+    private StudentRepository studentRepository;
+
     public StudentServiceImpl(StudentRepository studentRepository) {
-        super();
         this.studentRepository = studentRepository;
     }
-
-    private StudentRepository studentRepository;
 
     @Override
     public List<Student> getAllStudents() {
