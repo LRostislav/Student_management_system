49d43ce500be115a7db3b593c47c4d45c90ccaca
LRostislav <rostislav.lysenkov24@gmail.com>
2023-08-18 23:28:39 +0300

    add new student

--- src/main/java/com/example/studentmanagementsystem/StudentManagementSystemApplication.java
+++ src/main/java/com/example/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,23 +1,27 @@
 package com.example.studentmanagementsystem;
 
+import com.example.studentmanagementsystem.dto.StudentDto;
+import com.example.studentmanagementsystem.entity.Student;
+import com.example.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner{
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
 
-//    @Autowired
-//    private StudentRepository studentRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//        Student student3 = new Student("Nike", "Less", "lNike@yandex.ru");
-//        studentRepository.save(student3);
-//    }
+    @Override
+    public void run(String... args) throws Exception {
+
+        Student student3 = new Student(Long. parseLong("3"), "Nike", "Less", "lNike@yandex.ru");
+        studentRepository.save(student3);
+    }
 }
